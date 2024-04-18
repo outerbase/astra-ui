c93a59feb5e3b0fea6419e1131d52fa0d9a73a84
Johnny Domino <jvdomino@gmail.com>
2024-04-18 16:21:47 -0400

    update build process

---
+++ .config/rollup.config.js
@@ -0,0 +1,21 @@
+import commonjs from '@rollup/plugin-commonjs'
+import multi from '@rollup/plugin-multi-entry'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+
+export default {
+    // input: './out/js/index.js', // Point this to the file where your component is defined
+    input: './out/js/components/**.js',
+    output: {
+        // name: 'Astra', // Name of the global variable to hold the module's default export
+        file: 'out/web/astra.bundle.js',
+        // dir: 'out/web',
+        format: 'iife', // "iife" format for a self-executing function, suitable for inclusion as a <script> tag
+    },
+    plugins: [
+        multi(),
+        resolve(), // Resolves node modules
+        commonjs(), // Converts CommonJS modules to ES6, so they can be included in a Rollup bundle
+        terser(), // Minifies the bundle
+    ],
+}
