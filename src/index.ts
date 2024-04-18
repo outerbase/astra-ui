0ae805b83012fbda3beff1fad288b6d95793093a
Johnny Domino <jvdomino@gmail.com>
2024-04-18 15:46:36 -0400

    one file to export them all

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import AstraButton from './components/button'
+import AstraCard from './components/card'
+import AstraInput from './components/input'
+import AstraLabel from './components/label'
+import AstraSelect from './components/select'
+
+export { AstraButton, AstraCard, AstraInput, AstraLabel, AstraSelect }
