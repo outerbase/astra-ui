d647c279132959ddc0492067b4d3f3d1881a9186
Johnny Domino <jvdomino@gmail.com>
2024-04-23 16:22:03 -0400

    exclude `pages/*` usage in distributed tailwind css

--- .build/tailwind/tailwind.config.distribute.mjs
+++ .build/tailwind/tailwind.config.distribute.mjs
@@ -3,4 +3,5 @@ const baseConfig = require('../../tailwind.config.mjs')
 export default {
     ...baseConfig,
     darkMode: 'class',
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', '!./src/pages'],
 }
