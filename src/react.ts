cae1ef2f42e0084a8a7dfc0889991fa6f2eb15bc
Johnny Domino <jvdomino@gmail.com>
2024-06-11 16:24:37 -0400

    update charts

--- src/react.ts
+++ src/react.ts
@@ -4,6 +4,8 @@ import { createComponent } from '@lit/react'
 
 import AstraButton from './components/button.js'
 import AstraCard from './components/card.js'
+import AstraChart from './components/charts/chart.js'
+import AstraComposedChart from './components/charts/composed.js'
 import AstraInput from './components/input.js'
 import AstraScrollArea from './components/scroll-area.js'
 import AstraSelect from './components/select.js'
@@ -45,6 +47,16 @@ export default function LitComponents(React: any) {
     Table: createComponent({
       tagName: 'astra-table',
       elementClass: AstraTable,
+      react: React,
+    }),
+    ComposedChart: createComponent({
+      tagName: 'astra-composed-chart',
+      elementClass: AstraComposedChart,
+      react: React,
+    }),
+    Chart: createComponent({
+      tagName: 'astra-chart',
+      elementClass: AstraChart,
       react: React,
     }),
   }
