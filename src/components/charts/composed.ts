37922d41991da3aed853d5d1ead0ee3f205d9e45
Johnny Domino <jvdomino@gmail.com>
2024-06-20 13:24:54 -0400

    add `size` param

--- src/components/charts/composed.ts
+++ src/components/charts/composed.ts
@@ -7,16 +7,17 @@ import AstraChart from './chart.js'
 export default class AstraComposedChart extends AstraChart {
   @property({ type: String }) header?: string
   @property({ type: String }) subheader?: string
-  // TODO add property that determines the layout desired (sm, md, lg)
+  @property({ type: String }) size: 'small' | 'medium' | 'large' = 'large'
 
   public render() {
+    // include header when large
     const headerSection = html`
       <div id="header" class="flex justify-between w-full">
         ${this.header || this.subheader
           ? html`
               <div id="header-labels" class="flex flex-col gap-2">
                 ${this.header ? html`<h1 class="text-xl font-semibold">${this.header}</h1>` : null}
-                ${this.subheader ? html`<h2 class="text-md">${this.subheader}</h2>` : null}
+                ${this.subheader && this.size === 'large' ? html`<h2 class="text-md">${this.subheader}</h2>` : null}
               </div>
             `
           : null}
@@ -24,30 +25,38 @@ export default class AstraComposedChart extends AstraChart {
         <div class="hidden group-hover/actions:block"><slot name="actions"></slot></div>
       </div>
     `
-    const highlightSection = html`
-      <div class="flex w-full items-center justify-between gap-8">
-        <div
-          class="flex flex-auto items-center justify-center rounded bg-neutral-100 py-3 text-xl text-black dark:bg-neutral-900 dark:text-white"
-        >
-          27%
-        </div>
 
-        <div
-          class="flex flex-auto items-center  justify-center rounded bg-neutral-100 py-3 text-xl text-black dark:bg-neutral-900 dark:text-white"
-        >
-          17 hours
-        </div>
+    // include highlights when lg or md
+    const highlightSection =
+      this.size === 'large' || this.size === 'medium'
+        ? html`
+            <div id="highlights" class="flex w-full items-center justify-between gap-8">
+              <div
+                class="flex flex-auto items-center justify-center rounded bg-neutral-100 py-3 text-xl text-black dark:bg-neutral-900 dark:text-white"
+              >
+                27%
+              </div>
 
-        <div
-          class="flex flex-auto items-center  justify-center rounded bg-neutral-100 py-3 text-xl text-black dark:bg-neutral-900 dark:text-white"
-        >
-          🏎️
-        </div>
-      </div>
-    `
+              <div
+                class="flex flex-auto items-center  justify-center rounded bg-neutral-100 py-3 text-xl text-black dark:bg-neutral-900 dark:text-white"
+              >
+                17 hours
+              </div>
+
+              <div
+                class="flex flex-auto items-center  justify-center rounded bg-neutral-100 py-3 text-xl text-black dark:bg-neutral-900 dark:text-white"
+              >
+                🏎️
+              </div>
+            </div>
+          `
+        : null
+
+    // render chart
     const chart = super.render()
     const chartSection = html`<div id="chart" class="flex-1 p-2 flex items-center justify-center min-h-96 ">${chart}</div>`
-    console.log('composed theme', this.theme)
+
+    // render result
     return html`
       <div class="${classMap({ dark: this.theme === 'dark' })}">
         <div
