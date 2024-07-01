513e53dcf4f29f2aea796e1b2c827c71a68b406a
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2024-07-01 15:35:42 -0400

    Submit plugin value differently than default cell value

--- src/components/table/td.ts
+++ src/components/table/td.ts
@@ -409,27 +409,28 @@ export class TableData extends MutableElement {
       dark: this.theme === 'dark',
     })
 
-    if (value && typeof value === 'string') {
-      // Replace single, double, and backticks with their HTML entity equivalents
-      value = value.replace(/'/g, '&#39;').replace(/"/g, '&quot;').replace(/`/g, '&#96;')
-    }
-
-    if (editorValue && typeof editorValue === 'string') {
-      // Replace single, double, and backticks with their HTML entity equivalents
-      editorValue = editorValue.replace(/'/g, '&#39;').replace(/"/g, '&quot;').replace(/`/g, '&#96;')
-    }
-
     let cellContents: TemplateResult<1>
     let cellEditorContents: DirectiveResult<typeof UnsafeHTMLDirective> | undefined
 
     if (this.plugin) {
+      let pluginValue = value
+      if (value && typeof value === 'string') {
+        // Replace single, double, and backticks with their HTML entity equivalents
+        pluginValue = value.replace(/'/g, '&#39;').replace(/"/g, '&quot;').replace(/`/g, '&#96;')
+      }
+
+      if (editorValue && typeof editorValue === 'string') {
+        // Replace single, double, and backticks with their HTML entity equivalents
+        editorValue = editorValue.replace(/'/g, '&#39;').replace(/"/g, '&quot;').replace(/`/g, '&#96;')
+      }
+
       const { config, tagName } = this.plugin
 
       // TODO the plugin receives `null` as a string 'null' since params are always stringified
       //      we can resolve this by omitting `cellvalue` to represent null, but as of today, that renders `undefined` in our plugins
       //      `<${tagName} ${value !== null ? `cellvalue='${value}` : ''} configuration='${config}' ${this.pluginAttributes}></${tagName}>`
       const pluginAsString = unsafeHTML(
-        `<${tagName} cellvalue='${value}' columnName='${this.column}'  configuration='${config}' ${this.pluginAttributes}></${tagName}>`
+        `<${tagName} cellvalue='${pluginValue}' columnName='${this.column}'  configuration='${config}' ${this.pluginAttributes}></${tagName}>`
       )
 
       cellContents = html`${pluginAsString}`
