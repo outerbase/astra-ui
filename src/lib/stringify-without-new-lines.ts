2d5a8ac4f171af7a626e91af38a685f66c7d0fe2
Johnny Domino <jvdomino@gmail.com>
2024-07-18 18:05:15 -0400

    embellish empty objects with an extra space; `{ }`

--- src/lib/stringify-without-new-lines.ts
+++ src/lib/stringify-without-new-lines.ts
@@ -1,10 +1,17 @@
 export default function stringifyWithoutNewLines(obj: any, spaceCount = 1): string {
   // Convert the object to JSON string with specified spaces
-  const jsonString = JSON.stringify(obj, null, spaceCount)
+  let jsonString = JSON.stringify(obj, null, spaceCount)
 
   // Create a string with the specified number of spaces or a single space if spaceCount is greater than 0
   const spaces = spaceCount > 0 ? ' '.repeat(spaceCount) : ''
 
   // Replace all new lines and the spaces that follow them with the desired spaces
-  return jsonString.replace(/\n\s*/g, spaces)
+  jsonString = jsonString.replace(/\n\s*/g, spaces)
+
+  // Special case: format empty objects as `{ }`
+  if (jsonString === '{}') {
+    return '{ }'
+  }
+
+  return jsonString
 }
