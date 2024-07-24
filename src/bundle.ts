14252d5cdd87cd0a806d7daec94af763bfec4f16
Brayden Wilmoth <brayden.wilmoth@gmail.com>
2024-07-24 11:46:00 -0400

    Tab open event support

--- src/bundle.ts
+++ src/bundle.ts
@@ -3,10 +3,9 @@
 import AstraButton from './components/button.js'
 import AstraCard from './components/card.js'
 import AstraInput from './components/input.js'
-import OuterbaseTable from './components/outerbase-table.js'
 import AstraScrollArea from './components/scroll-area.js'
 import AstraSelect from './components/select.js'
 import AstraTable from './components/table/index.js'
 import AstraText from './components/text.js'
 
-export { AstraButton, AstraCard, AstraInput, AstraScrollArea, AstraSelect, AstraTable, AstraText, OuterbaseTable }
+export { AstraButton, AstraCard, AstraInput, AstraScrollArea, AstraSelect, AstraTable, AstraText }
