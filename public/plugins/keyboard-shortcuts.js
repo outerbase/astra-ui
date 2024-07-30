f29d7eab2e473f2f732aeac4659bf1f28c0db603
Johnny Domino <jvdomino@gmail.com>
2024-07-30 01:07:17 -0400

    remove erroneous file

--- public/plugins/keyboard-shortcuts.js
+++
@@ -1,9 +0,0 @@
-import type { UniverseEditor } from '../../src/components/universe'
-
-export default class UniversePlugin {
-  editor: UniverseEditor
-
-  constructor(editor: UniverseEditor) {
-    this.editor = editor
-  }
-}
