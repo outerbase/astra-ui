03922f7af88d8750f1aab1615c53e6c6fa6e1777
Johnny Domino <jvdomino@gmail.com>
2024-10-04 12:06:52 -0400

    add cell hovering

--- src/components/table/core/td.ts
+++ src/components/table/core/td.ts
@@ -255,7 +255,7 @@ export class TableData extends MutableElement {
       'bg-theme-table-row-selected dark:bg-theme-table-row-selected-dark':
         !this.rowIsNew && this.isActive && (!this.dirty || this.hideDirt), // i.e. this is the column being sorted
       'bg-theme-table-cell-dirty dark:bg-theme-table-cell-dirty-dark': !this.rowIsNew && this.dirty && !this.hideDirt, // dirty cells
-      'group-hover:bg-theme-table-row-hover dark:group-hover:bg-theme-table-row-hover-dark': !this.dirty || this.hideDirt,
+      'hover:bg-theme-table-row-hover dark:hover:bg-theme-table-row-hover-dark': !this.dirty || this.hideDirt,
       'focus:shadow-ringlet dark:focus:shadow-ringlet-dark focus:rounded-[4px] focus:ring-1 focus:ring-black dark:focus:ring-neutral-300 focus:outline-none':
         !this.isEditing && this.isInteractive,
       'border-r':
