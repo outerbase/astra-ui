1fad6f2dca93c8d020bc2dabc5dbafe6170f69f3
Johnny D <jvdomino@gmail.com>
2024-12-05 14:29:07 -0500

    reformat file

--- src/components/charts/composed.ts
+++ src/components/charts/composed.ts
@@ -171,48 +171,29 @@ export default class AstraComposedChart extends AstraChart {
     >
       ${showNoDataLabel
         ? html`<svg
-                  width="32"
-                  height="32"
-                  stroke=${this.theme === 'dark' ? '#e5e5e5' : '#262626'}
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                    <g transform-origin="center">
-                        <circle
-                            cx="12"
-                            cy="12"
-                            r="9.5"
-                            fill="none"
-                            stroke-width="1.5"
-                            stroke-linecap="round"
-                        >
-                            <animateTransform
-                                attributeName="transform"
-                                type="rotate"
-                                from="0 12 12"
-                                to="360 12 12"
-                                dur="2s"
-                                repeatCount="indefinite"
-                            />
+            width="32"
+            height="32"
+            stroke=${this.theme === 'dark' ? '#e5e5e5' : '#262626'}
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g transform-origin="center">
+              <circle cx="12" cy="12" r="9.5" fill="none" stroke-width="1.5" stroke-linecap="round">
+                <animateTransform attributeName="transform" type="rotate" from="0 12 12" to="360 12 12" dur="2s" repeatCount="indefinite" />
 
-                            <animate
-                                attributeName="stroke-dasharray"
-                                values="0 150;42 150;42 150"
-                                keyTimes="0;0.5;1"
-                                dur="1.5s"
-                                repeatCount="indefinite"
-                            />
+                <animate
+                  attributeName="stroke-dasharray"
+                  values="0 150;42 150;42 150"
+                  keyTimes="0;0.5;1"
+                  dur="1.5s"
+                  repeatCount="indefinite"
+                />
 
-                            <animate
-                                attributeName="stroke-dashoffset"
-                                values="0;-16;-59"
-                                keyTimes="0;0.5;1"
-                                dur="1.5s"
-                                repeatCount="indefinite"
-                            />
-                        </circle>
-                    </g>
-                </svg>`: html`${chart }`}
+                <animate attributeName="stroke-dashoffset" values="0;-16;-59" keyTimes="0;0.5;1" dur="1.5s" repeatCount="indefinite" />
+              </circle>
+            </g>
+          </svg>`
+        : html`${chart}`}
     </div>`
 
     // render result
