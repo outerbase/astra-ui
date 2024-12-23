33bdab5a540cfa6c52c28f01d2f86c9b41570391
Johnny Domino <jvdomino@gmail.com>
2024-12-23 16:41:41 -0500

    remove workspace junk

--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,1 @@
 export const OUTERBASE_API_DOMAIN = 'app.outerbase.com'
-
-// the following placeholder is used for demo/poc only
-// we are waiting for an API endpoint that only requires an auth token and chart id
-export const PLACEHOLDER_WORKSPACE_ID = '-est' // TODO not use a workspace id at all
